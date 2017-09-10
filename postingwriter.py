782a7f3b38f9af646f9762a71e2fed3cc27d9820
Leon Wetzel <leonwetzel@gmail.com>
2017-09-10 14:27:16 -0400

    git fucked up

--- postingwriter.py
+++ postingwriter.py
@@ -14,25 +14,54 @@ import fileinput
 import re
 import string
 
-# Initiate dictionaries and set
 dictionary = {}
-new = set()
-postings = set()
-words = {}
+postings = {}
+
 
 # Use regular expressions to cut every line in usable pieces and
-# only grab the words. These words will be put on separate lines.
+# only grab the words.
 for line in fileinput.input():
     # APPLE and apple should be the same, for instance ;)
     line = line.lower()
     try:
         split = re.search('^(\d*)(\t{1})(\S+)(\t{1})(.+)(\t{1})(.+)$', line)
-        new = split.group(7)
-        new = new.replace(" ", "\n")
-        new = new.translate(None, string.punctuation)
 
-        print(new)
-        postings.update(new)
+        # Retrieve tweet identifier
+        # - also known as key
+        identifier = split.group(1)
+        # print("\n\nID -> " + identifier)
+
+        # Generate tokens and place them in set 'new'
+        new = split.group(7).translate(None, string.punctuation).split()
+        # print("NEW ->")
+        # print(new)
+
+        # Dictionary structure:
+        # Key = word
+        # Value = posting list (i.e. tweet identifier(s) in which the word occurs)
+
+        # Example structure of dictionary:
+        # example = {"the": {902078295159828480, 902078295499526144}}
+        # - where example is a dictionary which will be inserted into dictionary postings
+
+        # new = set of words in a tweet
+        for token in new:
+            # print("TOKEN -> " + token)
+            if token in postings:
+                # Update the set of identifiers of a certain token in the dictionary
+                identifiers = postings[token]
+                identifiers.add(identifier)
+                postings.update({token: identifiers})
+
+                # print("POSTING1 ->")
+                # print(postings[token])
+            else:
+                # Insert a new token into the dictionary
+                item = {token: {identifier}}
+                postings.update(item)
+
+                # print("POSTING2 ->")
+                # print(postings[token])
 
         dictionary.update({split.group(1): (split.group(3), split.group(5), split.group(7))})
 
@@ -40,6 +69,12 @@ for line in fileinput.input():
         # Just in case ;)
         print(e)
 
-# Saves dictionary as pickle
+# Saves posting list dictionary as pickle
+with open('postinglist.pickle', 'wb') as p:
+    pickle.dump(postings, p)
+    print("Posting list successfully dumped to pickle!")
+
+# Saves tweets dictionary as pickle
 with open('tweets.pickle', 'wb') as f:
     pickle.dump(dictionary, f)
+    print("Tweets successfully dumped to pickle!")
