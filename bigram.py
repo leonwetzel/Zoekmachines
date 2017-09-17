b5ea68af6db9e54ca865a4f7f30a028888f2cacd
Leon <leonwetzel@gmail.com>
2017-09-17 05:57:39 -0400

    Finished bigram

--- bigram.py
+++ bigram.py
@@ -8,7 +8,6 @@ University of Groningen / Rijksuniversiteit Groningen
 
 l.f.a.wetzel@student.rug.nl
 """
-import sys
 
 
 def bigram(l1, l2):
@@ -18,24 +17,38 @@ def bigram(l1, l2):
     a position p+1 in the second list. In all other cases the function returns "False".
     Your solution should exploit the fact that both lists are ordered.
     """
-    try:
-        for line in sys.stdin:
-            # check if lengths of lists are equal
-            # if not equal, return false
-            if len(l1) != len(l2):
-                return False
+    # This was an attempt to implement a less naive way of intersection...
+    # it1 = iter(l1)
+    # it2 = iter(l2)
+    # sol = False
+    #
+    # n1 = next(it1)
+    # n2 = next(it2)
+    # while True:
+    #     try:
+    #         if n1 == n2 + 1:
+    #             return True
+    #         else:
+    #             if n1 < n2:
+    #                 n1 = next(it1)
+    #             else:
+    #                 n2 = next(it2)
+    #     except StopIteration:
+    #         return sol
 
-            for p in l1:
-                for j in l2:
-                    if j == p + 1:
-                        return True
-            return False
+    # Below is a naive way of intersection. However, it works...
+    sol = False
+    try:
+        # naive implementation
+        for p in l1:
+            for j in l2:
+                if j == p + 1:
+                    sol = True
+        print(sol)
 
     except TypeError:
-        sys.stderr.write("Missing lists!")
-        return False
+        print(sol)
 
 
 if __name__ == "__main__":
-    import sys
-    bigram()
+    bigram(l1=[], l2=[])
