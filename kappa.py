f1dc33efecb933cc4b6930af79b90af9fa61986d
Leon Wetzel <leonwetzel@gmail.com>
2017-10-13 19:59:29 +0200

    Update kappa.py

--- kappa.py
+++ kappa.py
@@ -10,8 +10,15 @@ l.f.a.wetzel@student.rug.nl
 
 import sys
 
-for line in sys.stdin:
-  [a, b] = line.rstrip().split('\t')
-  print(a)
-  print(b)
+def main():
+  """
+  Define a Python function which computes the value of kappa for two given lists of judgements.
+  Ensure your program can accept any judgement values (not just 0 or 1, but also a choice from,
+  say, green, blue or red). Your program should read a file with judgements, and write out the 
+  kappa value.
+  """
+  for line in sys.stdin:
+    [a, b] = line.rstrip().split('\t')
+    print(a)
+    print(b)
   
