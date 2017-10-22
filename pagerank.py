3b450283a20a9054df1d0f7ea069079e025bd6b3
Leon <leonwetzel@gmail.com>
2017-10-22 11:15:48 -0400

    IT WORKS!

--- pagerank.py
+++ pagerank.py
@@ -7,8 +7,8 @@ Search Engines / Zoekmachines
 University of Groningen / Rijksuniversiteit Groningen
 l.f.a.wetzel@student.rug.nl
 """
-
 import sys
+import numpy
 
 
 def main():
@@ -23,8 +23,70 @@ def main():
     """
     matrix = []
     for line in sys.stdin:
-        matrix.append([str(x) for x in line.rstrip().split('\t')])
-    print(matrix)
+        try:
+            matrix.append([float(x) for x in line.rstrip().split('\t')])
+        except ValueError:
+            matrix.append([float(x) for x in line.rstrip().split()])
+
+    standard_vector = generate_standard_vector(matrix)
+    pr = pagerank(standard_vector, matrix)
+    print(*pr, sep="\t")
+
+
+def pagerank(standard, matrix):
+    """
+    Calculates pagerank, based on a standard vector and a given matrix.
+    
+    :param standard: 
+    :param matrix: 
+    :return: 
+    """
+    new = []
+    mat = []
+
+    while new != standard:
+        if new:
+            standard = new.copy()
+            new.clear()
+            mat.clear()
+
+        for i, row in enumerate(matrix):
+            rij = []
+            for j, item in enumerate(row):
+                rij.append(item * standard[i])
+            mat.append(rij)
+
+        for i, row in enumerate(mat):
+            new.append(round(sum([item[i] for item in mat]), 4))
+
+    return new
+
+
+def generate_standard_vector(matrix):
+    """
+    Generates a standard vector based on given matrix length.
+    :param matrix: 
+    :return: 
+    """
+    vector = []
+    for item in matrix:
+        vector.append(1/len(matrix))
+    vector = fix_stochastic(vector)
+    return vector
+
+
+def fix_stochastic(vector):
+    """
+    Make sure that vector is right stochastic.
+    Applies correction if vector is not right stochastic.
+    :param vector: 
+    :return: (fixed) vector
+    """
+    if sum(vector) != 1:
+        vector.pop()
+        fix = 1 - sum(vector)
+        vector.append(fix)
+    return vector
 
 if __name__ == "__main__":
     main()
